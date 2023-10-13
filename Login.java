afca5f81e4f3d11fc9cf11badd6cd3586d6c1d72
Nageswara Rao P <nageswara.rao@wiculty.com>
2023-10-13 21:25:48 +0530

    print numbers

--- Login.java
+++ Login.java
@@ -1,8 +1,11 @@
-lsfjs
-fsfs
-f
-sf
-s
-fsfsf
-sff
-s
+public class Login {
+	
+	public static void main(String args[]) {
+		
+		int i;
+
+		for(i=1;i<=10;i++) {
+			System.out.println("Number: " + i);
+		}
+	}
+}
